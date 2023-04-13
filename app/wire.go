4993c976f88f8d6f15ea99334d060ab693c8a836
Adam Nasrudin <adamnasrudin858@gmail.com>
2023-04-13 12:07:31 +0700

    wiring inject service

--- app/wire.go
+++ app/wire.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"adamnasrudin03/to-do-list/app/repository"
+	"adamnasrudin03/to-do-list/app/service"
 
 	"gorm.io/gorm"
 )
@@ -9,5 +10,11 @@ import (
 func WiringRepository(db *gorm.DB) *repository.Repositories {
 	return &repository.Repositories{
 		Activity: repository.NewActivityRepository(db),
+	}
+}
+
+func WiringService(repo *repository.Repositories) *service.Services {
+	return &service.Services{
+		Activity: service.NewActivityService(repo.Activity),
 	}
 }
