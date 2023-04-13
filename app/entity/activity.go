249c7097e971716e2726fc91862e20fbd05391dd
Adam Nasrudin <adamnasrudin858@gmail.com>
2023-04-13 13:30:32 +0700

    fix typo

--- app/entity/activity.go
+++ app/entity/activity.go
@@ -4,7 +4,7 @@ import "time"
 
 type Activity struct {
 	ID        uint64    `gorm:"primaryKey" json:"id"`
-	Ttile     string    `gorm:"not null" json:"title"`
+	Title     string    `gorm:"not null" json:"title"`
 	Email     string    `gorm:"not null" json:"email" `
 	CreatedAt time.Time `gorm:"autoCreateTime" json:"createdAt"`
 	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"updatedAt"`
