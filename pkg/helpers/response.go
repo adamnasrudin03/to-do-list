20f44c94235350f1c92d8828c4b1fc3bfc162f5c
Adam Nasrudin <adamnasrudin858@gmail.com>
2023-04-13 12:19:55 +0700

    helpers format error validator

--- pkg/helpers/response.go
+++ pkg/helpers/response.go
@@ -1,5 +1,12 @@
 package helpers
 
+import (
+	"fmt"
+	"strings"
+
+	"github.com/go-playground/validator/v10"
+)
+
 type ResponseDefault struct {
 	Status  string      `json:"status"`
 	Message string      `json:"message"`
@@ -14,4 +21,28 @@ func APIResponse(message string, status string, data interface{}) ResponseDefaul
 		Message: message,
 		Data:    data,
 	}
+}
+
+// FormatValidationError func which holds errors during user input validation
+func FormatValidationError(err error) string {
+	var errors string
+
+	for _, e := range err.(validator.ValidationErrors) {
+		if errors != "" {
+			errors = fmt.Sprintf("%v, ", strings.TrimSpace(errors))
+		}
+
+		if e.Tag() == "email" {
+			errors = errors + fmt.Sprintf("%v must be type %v", e.Field(), e.Tag())
+		} else {
+			errors = errors + fmt.Sprintf("%v is %v %v", e.Field(), e.Tag(), e.Param())
+		}
+
+		if e.Param() != "" && e.Type().Name() == "string" {
+			errors = errors + " character"
+		}
+
+	}
+
+	return strings.TrimSpace(errors) + "."
 }
