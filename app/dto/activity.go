75ff0d7506be839cb4ce9de1afdab9c5919c4c9e
Adam Nasrudin <adamnasrudin858@gmail.com>
2023-04-13 14:31:26 +0700

    fix validate struct

--- app/dto/activity.go
+++ app/dto/activity.go
@@ -9,5 +9,5 @@ type CreateActivity struct {
 // Struct req Update activity
 type UpdateActivity struct {
 	Title string `json:"title"`
-	Email string `json:"email" validate:"email"`
+	Email string `json:"email" validate:"omitempty,email"`
 }
