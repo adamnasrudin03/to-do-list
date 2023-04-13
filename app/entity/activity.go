38e056e25bf3a173690a2c94fa5f38592161dad1
Adam Nasrudin <adamnasrudin858@gmail.com>
2023-04-13 12:05:44 +0700

    entity activity

---
+++ app/entity/activity.go
@@ -0,0 +1,11 @@
+package entity
+
+import "time"
+
+type Activity struct {
+	ID        uint64    `gorm:"primaryKey" json:"id"`
+	Ttile     string    `gorm:"not null" json:"title"`
+	Email     string    `gorm:"not null" json:"email" `
+	CreatedAt time.Time `gorm:"autoCreateTime" json:"createdAt"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"updatedAt"`
+}
