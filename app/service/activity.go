fb9f9641c0db2c9ddfd56d32c4eba564052fcd99
Adam Nasrudin <adamnasrudin858@gmail.com>
2023-04-13 12:06:22 +0700

    service activity

---
+++ app/service/activity.go
@@ -0,0 +1,104 @@
+package service
+
+import (
+	"adamnasrudin03/to-do-list/app/entity"
+	"adamnasrudin03/to-do-list/app/repository"
+	"errors"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+type ActivityService interface {
+	Create(ctx *gin.Context, input entity.Activity) (res entity.Activity, statusCode int, err error)
+	GetAll(ctx *gin.Context) (result []entity.Activity, statusCode int, err error)
+	GetByID(ctx *gin.Context, ID uint64) (result entity.Activity, statusCode int, err error)
+	UpdateByID(ctx *gin.Context, ID uint64, input entity.Activity) (result entity.Activity, statusCode int, err error)
+	DeleteByID(ctx *gin.Context, ID uint64) (statusCode int, err error)
+}
+
+type activitySrv struct {
+	ActivityRepository repository.ActivityRepository
+}
+
+func NewActivityService(ActivityRepo repository.ActivityRepository) ActivityService {
+	return &activitySrv{
+		ActivityRepository: ActivityRepo,
+	}
+}
+
+func (srv *activitySrv) Create(ctx *gin.Context, input entity.Activity) (res entity.Activity, statusCode int, err error) {
+	res, err = srv.ActivityRepository.Create(ctx, input)
+	if err != nil {
+		log.Printf("[ActivityService-Create] error create data: %+v \n", err)
+		return res, http.StatusInternalServerError, err
+	}
+
+	return res, http.StatusCreated, nil
+}
+
+func (srv *activitySrv) GetAll(ctx *gin.Context) (result []entity.Activity, statusCode int, err error) {
+	result, err = srv.ActivityRepository.GetAll(ctx)
+	if err != nil {
+		log.Printf("[ActivityService-GetAll] error get data repo: %+v \n", err)
+		return result, http.StatusInternalServerError, err
+	}
+
+	return result, http.StatusOK, nil
+}
+
+func (srv *activitySrv) GetByID(ctx *gin.Context, ID uint64) (result entity.Activity, statusCode int, err error) {
+	result, err = srv.ActivityRepository.GetByID(ctx, ID)
+	if errors.Is(err, gorm.ErrRecordNotFound) || result.ID == 0 {
+		return result, http.StatusNotFound, err
+	}
+
+	if err != nil {
+		log.Printf("[ActivityService-GetByID] error get data repo: %+v \n", err)
+		return result, http.StatusInternalServerError, err
+	}
+
+	return result, http.StatusOK, nil
+}
+
+func (srv *activitySrv) UpdateByID(ctx *gin.Context, ID uint64, input entity.Activity) (result entity.Activity, statusCode int, err error) {
+	sm, err := srv.ActivityRepository.GetByID(ctx, ID)
+	if errors.Is(err, gorm.ErrRecordNotFound) || sm.ID == 0 {
+		return result, http.StatusNotFound, err
+	}
+
+	if err != nil {
+		log.Printf("[ActivityService-UpdateByID] error get data repo: %+v \n", err)
+		return result, http.StatusInternalServerError, err
+	}
+
+	result, err = srv.ActivityRepository.UpdateByID(ctx, ID, input)
+	if err != nil {
+		log.Printf("[ActivityService-UpdateByID] error update data repo: %+v \n", err)
+		return result, http.StatusInternalServerError, err
+	}
+
+	return result, http.StatusOK, nil
+}
+
+func (srv *activitySrv) DeleteByID(ctx *gin.Context, ID uint64) (statusCode int, err error) {
+	sm, err := srv.ActivityRepository.GetByID(ctx, ID)
+	if errors.Is(err, gorm.ErrRecordNotFound) || sm.ID == 0 {
+		return http.StatusNotFound, err
+	}
+
+	if err != nil {
+		log.Printf("[ActivityService-DeleteByID] error get data repo: %+v \n", err)
+		return http.StatusInternalServerError, err
+	}
+
+	err = srv.ActivityRepository.DeleteByID(ctx, ID)
+	if err != nil {
+		log.Printf("[ActivityService-DeleteByID] error delete data repo: %+v \n", err)
+		return http.StatusInternalServerError, err
+	}
+
+	return http.StatusOK, nil
+}
---
+++ app/service/service.go
@@ -0,0 +1,6 @@
+package service
+
+// Services all service object injected here
+type Services struct {
+	Activity ActivityService
+}
